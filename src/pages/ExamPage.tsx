e53ef3862edc6c0ee9116b4114fe6662cf64e4ad
Marwa Elgorn <www.marwaelgorn@gmail.com>
2026-03-15 23:26:15 +0200

    initialize scalable project architecture for exam module

---
+++ src/pages/ExamPage.tsx
@@ -0,0 +1,9 @@
+
+import MainLayout from '../features/layout/components/MainLayout'
+export default function ExamPage() {
+  return (
+      <MainLayout>
+      exam content
+    </MainLayout>
+  )
+}
